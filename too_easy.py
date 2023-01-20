6ae42b5553cd716a55f59e4fe045c14266e7c6ae
HelenAV96 <helenalbandak@hotmail.com>
2023-01-20 15:14:24 +0100

    hej

--- too_easy.py
+++ too_easy.py
@@ -1,35 +1,7 @@
-import os
 import pandas as pd
-import json
-from pandas.io.json import json_normalize
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import psycopg2
-
-# with open('raw_to_harmonized\\2023-01-19_09-47-38_Umea.json') as json_file:
-#     data = json.load(json_file)
-
-# hourly = data['hourly']
-
-# df = pd.DataFrame(hourly)
-
-# plt.figure()
-# df.plot(x='time', y='temperature_2m', kind='line')
-
-# plt.xlabel('Time')
-# plt.ylabel('Temperature (°C)')
-# plt.show()
-
-
-# # data = pd.read_json("raw_to_harmonized\\2023-01-19_09-47-38_Umea.json")
-
-# plt.figure()
-# data.plot(x='time', y='temperature_2m', kind='line')
-# plt.xlabel('Time')
-# plt.ylabel('Temperature (°C)')
-# plt.show()
-
-#THIS IS THE ONE
 
 conn = psycopg2.connect(user="postgres",
                         password="musikklass96",
@@ -53,26 +25,30 @@ fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
 
 ax1.plot(time, temperature_2m, 'b-')
 ax1.set_xlabel('time')
-ax1.set_ylabel('temperature', color='b')
+ax1.set_ylabel('temperature_2m', color='b')
 ax1.tick_params('y', colors='b')
 ax1.xaxis.set_major_locator(mdates.HourLocator(byhour=range(0,24,24)))
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
 ax1.grid(True, axis='y', alpha=0.7)
 
-ax1.twinx().plot(time, relativehumidity_2m, 'r-')
-ax1.set_ylabel('humidity', color='r')
-ax1.tick_params('y', colors='r')
+ax1_right = ax1.twinx()
+ax1_right.plot(time, relativehumidity_2m, 'c-')
+ax1_right.set_ylabel('relativehumidity_2m', color='c')
+ax1_right.tick_params('y', colors='c')
 
-ax2.plot(time, precipitation, 'y-')
+ax2.plot(time, precipitation, 'k-')
 ax2.set_xlabel('time')
-ax2.set_ylabel('precipitation', color='y')
-ax2.tick_params('y', colors='y')
+ax2.set_ylabel('precipitation', color='k')
+ax2.tick_params('y', colors='k')
 ax2.xaxis.set_major_locator(mdates.HourLocator(byhour=range(0,24,24)))
 ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
 ax2.grid(True, axis='y', alpha=0.7)
 
-plt.title('Temperature, Humidity and Precipitation')
+plt.title('Umeå - Temperature, Humidity and Precipitation')
 plt.show()
+
+# plt.savefig("path/to/save/file.png")
 
 cur.close()
 conn.close()
+
