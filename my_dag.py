c88ae1e1fbd1639859b1040f6f90f93014d1db6b
slindberget <118910064+slindberget@users.noreply.github.com>
2023-01-20 14:33:12 +0100

    Lagt till nya etl och databas

--- my_dag.py
+++ my_dag.py
@@ -1,8 +1,14 @@
-from airflow import DAG
+import pandas as pd
+from datetime import datetime
+from airflow.models import DAG
 from airflow.operators.python import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 from simons_databasgrej import insert_data_to_dbs
-
+from etl_meteo import request_new_weather_data, transform, rename
 
 args = {
     'owner': 'Helen ALbandak',
@@ -19,8 +25,29 @@ dag = DAG(
 )
 
 with dag:
-    insert_data_to_dbs = PythonOperator(
-        task_id='insert_data_to_db',
-        python_callable= insert_data_to_dbs,
+
+    request_new_weather_data = PythonOperator(
+        task_id='request_new_weather_data',
+        python_callable= request_new_weather_data,
+        #provide_context=True,
+        do_xcom_push=True
+    )
+    transform = PythonOperator(
+        task_id='transform',
+        python_callable= transform,
+        do_xcom_push=True
         # provide_context=True
     )
+    rename= PythonOperator(
+        task_id='rename',
+        python_callable= rename,
+        do_xcom_push=True
+        # provide_context=True
+    )
+
+    # insert_data_to_dbs = PythonOperator(
+    #     task_id='insert_data_to_db',
+    #     python_callable= insert_data_to_dbs,
+   #      do_xcom_push=True
+    #     # provide_context=True
+    # )
--- simons_databasgrej_maste_ligga_i_dags.py
+++ simons_databasgrej_maste_ligga_i_dags.py
@@ -1,24 +1,23 @@
 import json
 import os
-
 import pandas as pd
 from sqlalchemy import create_engine
 
-
-
-
-
-engine = create_engine('postgresql://postgres:Ghznmqm7@localhost:5432/etl-meteo')
+engine = create_engine('postgresql+psycopg2://postgres:Ghznmqm7@localhost:5432/etl-meteo')
 folder_name = "raw_to_harmonized"
 table_names = ["weather_umea", "weather_sodertalje", "weather_trosa", "weather_varmdo"]
 
 def insert_data_to_dbs(folder_name, table_names):
+    # Get the current working directory
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    # Join the current working directory and the folder name
+    folder_path = os.path.join(cwd + "/" + folder_name)
     # Loop through the files in the folder
-    for filename in os.listdir(folder_name):
+    for filename in os.listdir(folder_path):
         # Extract the location from the file name
         location = filename.split('_')[2].split('.')[0].lower()
         # Open the file
-        with open(folder_name + '/' + filename) as json_file:
+        with open(os.path.join(folder_path, filename)) as json_file:
             data = json.load(json_file)
         # Convert json to dataframe
         df = pd.DataFrame(data)
@@ -27,9 +26,9 @@ def insert_data_to_dbs(folder_name, table_names):
         # Insert dataframe into sql table
         for table_name in table_names:
             if table_name.endswith(location):
-                df.to_sql(table_name, engine, if_exists='append', index=False)
+                df.to_sql(table_name, engine, if_exists='replace', index=False)
+                print(f"Data inserted into {table_name} table")
             else:
                 pass
-
 
 insert_data_to_dbs(folder_name,table_names)
